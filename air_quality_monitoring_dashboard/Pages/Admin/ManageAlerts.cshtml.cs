c8a24d318e3a4e87ea48f1a9a498c47242e0b4a0
Thenuri <thenugamage311@gmail.com>
2025-04-25 17:19:44 +0530

    creating the folder structure

---
+++ air_quality_monitoring_dashboard/Pages/Admin/ManageAlerts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace air_quality_monitoring_dashboard.Pages.Admin
+{
+    public class ManageAlertsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
