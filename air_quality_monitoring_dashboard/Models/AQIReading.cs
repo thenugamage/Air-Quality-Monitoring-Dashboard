ad938aa3b974bc9023221760ee5b4a174587b98d
Thenuri <thenugamage311@gmail.com>
2025-04-25 18:43:36 +0530

    creating the project and adding gitignore file

--- air_quality_monitoring_dashboard/Models/AQIReading.cs
+++ air_quality_monitoring_dashboard/Models/AQIReading.cs
@@ -2,5 +2,10 @@
 {
     public class AQIReading
     {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public Sensor Sensor { get; set; }
+        public int AQI { get; set; }
+        public DateTime Timestamp { get; set; }
     }
 }
--- air_quality_monitoring_dashboard/Models/Sensor.cs
+++ air_quality_monitoring_dashboard/Models/Sensor.cs
@@ -2,5 +2,12 @@
 {
     public class Sensor
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; } // Optional
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsActive { get; set; }
+        public List<AQIReading> Readings { get; set; }
     }
 }
